b9a2140abab081286ad5b4c3fd985bd0779de0ac
Tiramisush1 <martin.sandoval.escarate@alumnos.sip.cl>
2022-04-12 10:53:15 -0400

    test1

---
+++ Arreglos_Condiciones_Martin_Sandoval.js
@@ -0,0 +1,32 @@
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+
+/*
+Variable              Datos
+authContactInfo       ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345]
+*/
+
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+}
+
+/*
+variables         datos
+produce      =    ["manzanas", "naranjas"]
+frozen       =    ["brocoli", "helado"]
+*/
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+
+/*
+variable           datos
+movieLibrary      ["Bambi", "E.T.", "Toy Story"]
+*/
