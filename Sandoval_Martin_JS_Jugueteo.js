9abf05d7d8c0bd48a3546015700705900bf421cb
Tiramisush1 <martin.sandoval.escarate@alumnos.sip.cl>
2022-04-21 08:44:20 -0400

    up 1

--- Sandoval_Martin_JS_Jugueteo.js
+++ Sandoval_Martin_JS_Jugueteo.js
@@ -1,2 +1,11 @@
-var = AlturaNiño = 3;
-function = muestraSiElNiñoPuedeSubirALaMontañaRusa
+var alturaNiño = 45;
+function muestraSiElNiñoPuedeSubirALaMontañaRusa () {
+    if (alturaNiño >= 52) {
+        console.log ("¡Súbete, chico!")
+    }
+    else {
+        alturaNiño < 52; {
+            console.log ("Lo siento, chico. Tal vez el próximo año")
+        }
+    }
+}
