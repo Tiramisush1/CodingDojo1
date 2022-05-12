0531afe6e92e74aad5fd99598612b993a101f2dd
Tiramisush1 <martin.sandova.escarate@alumnos.sip.cl>
2022-05-12 17:53:38 -0400

    up1

---
+++ ninjaman/index.js
@@ -0,0 +1,74 @@
+var world = [
+    [1, 1, 1, 1, 1],
+    [1, 0, 2, 2, 1],
+    [1, 2, 1, 2, 1],
+    [1, 2, 2, 2, 1],
+    [1, 2, 2, 2, 1],
+    [1, 2, 1, 2, 1],
+    [1, 2, 2, 2, 1],
+    [1, 2, 2, 2, 1],
+    [1, 2, 1, 2, 1],
+    [1, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1],
+];
+var worldDict = {
+    0: "blank",
+    1: "wall",
+    2: "sushi",
+    3: "onigiri"
+}
+
+function drawWorld() {
+    var output = "";
+
+    for (var row = 0; row < world.length; row++) {
+        output += "<div class = 'row'></div>"
+        for (var x = 0; x < world[row].length; x++) {
+            output += "<div class = '" + worldDict[
+                world[row][x]] + "'></div>"
+        }
+        output += "</div>"
+    }
+    document.getElementById("world").innerHTML = output;
+}
+drawWorld(); {
+    var ninjaman = {
+        x: 1,
+        y: 1
+    }
+
+    function drawNinjaman() {
+        document.getElementById('ninjaman').style.top =
+            ninjaman.x * 40 + 'px'
+        document.getElementById('ninjaman').style.left =
+            ninjaman.x * 40 + 'px'
+    }
+}
+
+drawNinjaman()
+
+    document.onkeydown = function (e) {
+    switch (e.keyCode) {
+        case 37:
+            if (map[ninjaman.top][ninjaman.left - 1] !== 0) {
+                ninjaman.left--;
+            }
+            break;
+        case 39:
+            if (map[ninjaman.top][ninjaman.left + 1] !== 0) {
+                ninjaman.left++;
+            }
+            break;
+        case 38:
+            if (map[ninjaman.top - 1][ninjaman.left] !== 0) {
+                ninjaman.top--;
+            }
+            break;
+        case 40:
+            if (map[ninjaman.top + 1][ninjaman.left] !== 0) {
+                ninjaman.top++;
+            }
+            break;
+    }
+    drawNinjaman();
+};
