a21d433ca61c8285b9a2b3659a5718d5f0a1c5b5
Tiramisush1 <martin.sandoval.escarate@alumnos.sip.cl>
2022-05-19 12:24:45 -0400

    commit1

--- DesafioAvion/index.js
+++ DesafioAvion/index.js
@@ -1,12 +1,44 @@
 var player = {
-    left: 450,
+    left: 1,
     top: 620
 }
+
 var enemies = [
-    { left: 350, top: 200 },
-    { left: 450, top: 175 },
-    { left: 300, top: 100 },
-    { left: 500, top: 255 },
-    { left: 25, top: 375 },
-    { left: 300, top: 275 }
+    {left: 350, top: 200},
+    {left: 150, top: 220},
+    {left: 700, top: 220}
 ]
+
+function drawPlayer(){
+    content = "<div class='player' style='left:"+player.left+"px; top:"+player.top+"px'></div>";
+    document.getElementById("players").innerHTML = content;
+}
+drawPlayer();
+
+function drawEnemies(){
+    content = "";
+    console.log(enemies);
+    for(var idx=0; idx<enemies.length; idx++){
+        content += "<div class='enemy' style='left:"+enemies[idx].left+"px; top:"+enemies[idx].top+"px'></div>";
+    }
+    document.getElementById("enemies").innerHTML = content;
+}
+drawEnemies();
+
+
+document.onkeydown = function(e) {
+    console.log(e);
+    if(e.keyCode == 37 && player.left){ // left
+        player.left = player.left - 10;
+    }
+    if(e.keyCode == 39 && player.left){ // right
+        player.left = player.left + 10;
+    }
+    if(e.keyCode == 38 && player.top > 500){ // UP
+        player.top = player.top - 10;
+    }
+    if(e.keyCode == 40 && player.top < 625){ // DOWN
+        player.top = player.top + 10;
+    }
+    drawPlayer();
+}
