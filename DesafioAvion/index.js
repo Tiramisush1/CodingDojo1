5002f7d37fc1b9ebc7473b525d8c3567872a1747
Tiramisush1 <martin.sandoval.escarate@alumnos.sip.cl>
2022-05-19 10:47:06 -0400

    up2

---
+++ DesafioAvion/index.js
@@ -0,0 +1,12 @@
+var player = {
+    left: 450,
+    top: 620
+}
+var enemies = [
+    { left: 350, top: 200 },
+    { left: 450, top: 175 },
+    { left: 300, top: 100 },
+    { left: 500, top: 255 },
+    { left: 25, top: 375 },
+    { left: 300, top: 275 }
+]
