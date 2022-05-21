4476f15db3da5b2f8b293eebb2b25571c6788967
Tiramisush1 <martin.sandova.escarate@alumnos.sip.cl>
2022-05-20 21:42:19 -0400

    up1

--- DesafioAvion/index.js
+++ DesafioAvion/index.js
@@ -1,37 +1,55 @@
 var player = {
-    left: 1,
+    left: 10,
     top: 620
 }
 
 var enemies = [
-    {left: 350, top: 200},
-    {left: 150, top: 220},
-    {left: 700, top: 220}
+    {left: 700, top: 10},
+    {left: 300, top: 30},
+    {left: 120, top: 100},
 ]
 
-function drawPlayer(){
-    content = "<div class='player' style='left:"+player.left+"px; top:"+player.top+"px'></div>";
-    document.getElementById("players").innerHTML = content;
+var missiles = []
+
+    function drawPlayer(){
+        content = "<div class='player' style='left:"+player.left+"px; top:"+player.top+"px'></div>";
+        document.getElementById("player").innerHTML = content;
 }
-drawPlayer();
 
 function drawEnemies(){
     content = "";
-    console.log(enemies);
     for(var idx=0; idx<enemies.length; idx++){
         content += "<div class='enemy' style='left:"+enemies[idx].left+"px; top:"+enemies[idx].top+"px'></div>";
     }
     document.getElementById("enemies").innerHTML = content;
 }
-drawEnemies();
 
+function drawMissiles(){
+    content = "";
+    for(var idx=0; idx<missiles.length; idx++){
+        content += "<div class='missiles' style='left:"+missiles[idx].left+"px; top:"+missiles[idx].top+"px'></div>"
+    }
+    document.getElementById("missiles").innerHTML = content;
+}
+
+function moveEnemies(){
+    for(var idx=0; idx < enemies.length; idx++){
+        enemies[idx].top = enemies[idx++].top + 1;
+    }
+}
+
+function moveMissiles(){
+    for(var idx=0; idx<missiles.length; idx++){
+        missiles[idx].top = missiles[idx].top - 2;
+    }
+}
 
 document.onkeydown = function(e) {
     console.log(e);
-    if(e.keyCode == 37 && player.left){ // left
+    if(e.keyCode == 37 && player.left > 10){ // left
         player.left = player.left - 10;
     }
-    if(e.keyCode == 39 && player.left){ // right
+    if(e.keyCode == 39 && player.left < 839){ // right
         player.left = player.left + 10;
     }
     if(e.keyCode == 38 && player.top > 500){ // UP
@@ -40,5 +58,21 @@ document.onkeydown = function(e) {
     if(e.keyCode == 40 && player.top < 625){ // DOWN
         player.top = player.top + 10;
     }
+    if(e.keyCode == 32){
+        missiles.push({left: player.left + 34, top: player.top - 8})
+        drawMissiles();
+    }
     drawPlayer();
 }
+function gameLoop(){
+    console.log('gameLoop funciona')
+    drawPlayer();
+    
+    moveEnemies();
+    drawEnemies();
+    moveMissiles();
+    drawMissiles();
+
+    setTimeout(gameLoop, 10)
+}
+gameLoop();
